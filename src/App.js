7462959ddc4f7ddefc363e2324b97b7e8b31f32d
Chris Olmos <christianolmos@protonmail.com>
2020-10-01 16:55:35 -0400

    structure in place

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="Header"/>
+
+    <div class="flex-container">
+      <div className="column1">
+        <h2>Column 1</h2>
+        <div className="trackrecord"/> 
+      </div>
+      <div className="column2"><h2>Column 2</h2></div>
+      <div className="column3"><h2>Column 3</h2></div>
+    </div>
+
+
+    <div className="Footer"/>
+
+
+
+
     </div>
   );
 }
