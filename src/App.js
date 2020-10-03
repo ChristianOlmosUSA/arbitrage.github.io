71d0f2d50af8daefc4bd3ecb533b2294288069ba
Chris Olmos <christianolmos@protonmail.com>
2020-10-03 15:06:51 -0400

    FINISHED

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton } from 'r
 import logo from '../src/img/Arb_logo.png';
 import chris from '../src/img/Chris_circle.png';
 import rick from '../src/img/Rick_circle.png';
+import ContactsFooter from '../src/img/ContactsFooter.png';
 import './App.css';
 
 function App() {
@@ -58,9 +59,9 @@ function App() {
           <img></img>
           <p> As a journalist Rick built many of the same skills in seeking out information that he has used in his 12 year year career as a headhunter. He has been with Arbitrage since 2014</p>
           <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/robinsonrick/">Connect to Rick on Linkedin</a>
-
-
           </div>
+
+          <div class="WeHave"><p> We have recruited in London, New York, Hong Kong, Singapore, Germany, France, Cyprus, Palm Beach, Miami etc</p></div>
           
           
       </div>
@@ -79,33 +80,51 @@ function App() {
               <p>Top Tier Global Macro Hedge Fund seeks an analyst with 5-10 years of experience covering countries such as Indonesia and Philippines, forecasting GDP, Inflation and high frequency indicators</p>
           </div>
           
+          
           <div class="Job3Box">
           <h5> Asset Raiser, FX & Credit Hedge Fund</h5>
               <p>Sales analyst with 1-3 years to support a new head of team. Client with highly regarded reputation among hedge fund community</p>
           </div>
-          <div class="Job4Box">
+          <div class="pleaseconnect"><p> for all vacancies please connect to us on linkedin or send an email/call in</p></div>
+          <div class="Job4Box" onClick="">
           <h5> Portfolio Manager / Proprietary Trader, London or NY</h5>
               <p>Global Fund with Capital to deploy seeks only the best performing traders with strong track records. Min $100m+ book</p>
           </div>
+          
           <div class="Job5Box">
           <h5> Macro Research Sales</h5>
               <p>Independent Research firm with quality research output seeks to expand its sales presence, CSA / Subscription sales experience sought</p>
           </div>
           <div class="Job6Box">
-          <h5> Quant Analyst</h5>
+          <h5> Quantitative Analyst</h5>
               <p> Building High Frequency trading FX models, using python, MatLab and with awareness of FIX protocol and low latency technologies</p>
           </div>
-          <div class="RecruitWithUsBox"></div>
-          <div class="PastPlacementsBox"></div>
+          <div class="pleaseconnect"><p> for all vacancies please connect to us on linkedin or send an email/call in</p></div>
+          <div class="RecruitWithUsBox">
+          <h5> Request more information for a search</h5>
+              <p> Contact us to discuss different approaches in how to add alpha to your team. +44 203 823 4541.</p>
+
+
+          </div>
+          
       
       </div>
 
-      <div className="column3"><h2>Twitter</h2>
+      <div className="column3"><h2>Top Vacancies & Charts</h2>
+      <TwitterFollowButton
+    screenName={'arbitragesearch'}
+  />
       <TwitterTimelineEmbed
           sourceType="profile"
           screenName="arbitragesearch"
-          options={{height: 800}}
+          options={{height: 1010}}
+          
         />
+
+<div class="WeHave2"><p> Recruiting Economists, FX Strategist, Equity Strategist, Macro Portfolio Manager, Multi-Asset PM, EM Debt Analysts</p></div>
+       
+
+
       </div>
 
     </div>
@@ -113,7 +132,8 @@ function App() {
     </div>
 
     <div className="Footer">    
-    +44 203 823 .... | +1 786 818 ... | firstcontact@arbitrage-search....  
+    
+    <img className="logo" src={ContactsFooter} />
     </div>
 
 
